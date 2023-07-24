2cc914d59ce7cd80431be74c98b9a1c945c430b2
hiroka <mementoka@gmail.com>
2023-07-24 13:32:14 +0900

    add back action post

--- components/TransitionButtons.tsx
+++ components/TransitionButtons.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { postData } from  'utils/postData';
 
 function removeChoiceItems() {
   Object.keys(sessionStorage).forEach(key => {
@@ -12,8 +13,14 @@ const TransitionButtons = () => {
     <>
       <button 
         className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-1 rounded"
-        onClick={() => {
+        onClick={async () => {
           if (typeof sessionStorage === "undefined") return;
+          const backActionData = {
+            'state': '',
+            'action': 'back',
+            'actionType': 'back'
+          };
+          await postData(backActionData);
           if (sessionStorage.length !== 0) {
             const delIndex = sessionStorage.length - 1;
             sessionStorage.removeItem(delIndex.toString());
