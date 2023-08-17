286b0c82022c802ef02cbfd9d376ac62d2ed5c3c
hiroka <mementoka@gmail.com>
2023-08-17 16:37:41 +0900

    fix index

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,16 +24,18 @@ export default function Home() {
   return (
     <>
       <main className="px-6">
-        <p suppressHydrationWarning={true} className="m-1">ユーザ名: {userName}</p>
-        <button
-          onClick={inputUserName}
-          className="m-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
-          ユーザ名を入力
-        </button>
+        <div className="flex">
+          <p suppressHydrationWarning={true} className="m-2">ユーザ名: {userName}</p>
+          <button
+            onClick={inputUserName}
+            className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+            ユーザ名を入力
+          </button>
+        </div>
 
         <h1 className="text-2xl font-bold p-1">プログラム課題</h1>
-        <h1 className="text-lg">コード選択あり</h1>
-        <ul className="m-1 text-xl list-disc list-inside">
+        <h1 className="text-lg px-2">コード選択あり</h1>
+        <ul className="text-xl list-disc list-inside">
           <li>
             <Link
               href="/problem-01"
@@ -57,8 +59,8 @@ export default function Home() {
           </li>
         </ul>
 
-        <h1 className="text-lg pt-2">コード選択なし</h1>
-        <ul className="m-1 text-xl list-disc list-inside">
+        <h1 className="text-lg pt-2 px-2">コード選択なし</h1>
+        <ul className="text-xl list-disc list-inside">
           <li>
             <Link
               href="/policy-problem-01"
