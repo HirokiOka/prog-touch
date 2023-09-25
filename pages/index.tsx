0ce4952683af682096b53756aa76636c6fdf2dee
hiroka <mementoka@gmail.com>
2023-09-26 00:13:00 +0900

    Init problem 04

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react';
 
 export default function Home() {
   const [userName, setUserName] = useState('anonymous');
+  const problemNumbers: number[] = [1, 2];
+  const sectionTwo: number[] = [4, 3];
 
   const inputUserName = () => {
     const inputName: string = window.prompt('名前を入力してください:') ?? ''; 
@@ -34,30 +36,37 @@ export default function Home() {
         </div>
 
         <h1 className="text-2xl font-bold p-1">課題一覧</h1>
-        <ul className="text-xl list-disc list-inside">
-          <li>
-            <Link
-              href="/problem/1"
-              className="text-blue-500 hover:underline">
-              課題1
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/problem/2"
-              className="text-blue-500 hover:underline">
-              課題2
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/problem/3"
-              className="text-blue-500 hover:underline">
-              課題3
-            </Link>
-          </li>
+        <ul className="text-lg list-disc list-inside">
+          {problemNumbers.map((v, i) => { 
+            return (
+              <li key={i}>
+                <Link
+                  href={`/problem/${v}`}
+                  className="text-blue-500 hover:underline">
+                  課題{v}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
-
+        <h2 className="text-xl">1. はじめてのプログラミング</h2>
+        <h2 className="text-xl">2. 計算で図形を描く</h2>
+        <ul className="text-lg list-disc list-inside">
+          {sectionTwo.map((v, i) => { 
+            return (
+              <li key={i}>
+                <Link
+                  href={`/problem/${v}`}
+                  className="text-blue-500 hover:underline">
+                  section2-q{i+1}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+        <h2 className="text-xl">3. アニメーションとインタラクション</h2>
+        <h2 className="text-xl">4. たくさんの値をまとめて扱う</h2>
+        <h2 className="text-xl">5. 関数を使って長いプログラムを整理する</h2>
       </main>
     </>
   );
